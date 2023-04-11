35ef145ebc8f1e716f4e362d085ffcfd843a2a72
Fernando Costa Pereira <34512218+nandopera@users.noreply.github.com>
2023-04-11 16:34:55 -0300

    Clone done!

---
+++ pages/about/index.js
@@ -0,0 +1,11 @@
+$(document).ready(myAbout)
+
+/**
+ * Função principal da página "about".
+ **/
+function myAbout() {
+    /**
+     * Altera o título da página quando 'about' for acessada.
+     **/
+    changeTitle('Sobre...')
+}
---
+++ pages/home/index.js
@@ -0,0 +1,11 @@
+$(document).ready(myHome)
+
+/**
+ * Função principal da página "home".
+ **/
+function myHome() {
+    /**
+     * Altera o título da página quando 'home' for acessada.
+     **/
+    changeTitle()
+}
